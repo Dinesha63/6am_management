6e2393b531df6799f857ef6652ef9368009fcb82
Thirumoorthi <thirumoorthi@skitech.ai>
2025-08-20 11:43:21 +0530

    update auth api  url modify

--- src/redux/Features/Auth/authApi.ts
+++ src/redux/Features/Auth/authApi.ts
@@ -1,7 +1,8 @@
 import API from '../../../services/api';
 import { LoginPayload } from './auth.types';
-
+const BASE_URL = 'http://172.21.1.248:8080/6AM/Main/api/v1';
+const TEST_API_BASE_URL = 'https://testapi.skitech.ai/6AM/Main/api/v1';
 export const loginUserAPI = async (payload: LoginPayload) => {
-  const response = await API.post('/Employee/VerifyEmployeeLogin', payload);
+  const response = await API.post(`${BASE_URL}/Employee/VerifyEmployeeLogin`, payload);
   return response.data;
 };
